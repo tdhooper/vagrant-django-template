04a502b3e62e5a22ff665f8dd6b7cdf7065a7036
Thomas Hooper <thooper@Macintosh.local>
2014-05-03 19:47:21 +0100

    Move core into the project's apps and call it example

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -141,8 +141,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-
-    'core',
 )
 
 EMAIL_SUBJECT_PREFIX = '[{{ project_name }}] '
